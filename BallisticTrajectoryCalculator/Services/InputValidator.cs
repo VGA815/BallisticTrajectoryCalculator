7edb1eec2f79e6bc26ae716dd8023f3a42602947
VGA815 <109912901+VGA815@users.noreply.github.com>
2023-05-25 23:27:58 +0300

    Commit

--- BallisticTrajectoryCalculator/Services/InputValidator.cs
+++ BallisticTrajectoryCalculator/Services/InputValidator.cs
@@ -1,58 +1,49 @@
 ﻿namespace BallisticTrajectoryCalculator.Services
 {
     public class InputValidator
-    {
+    {        
         // bcBox Validate
         [Required]
-        [DisallowNull]
         public string? SelectedItemBCbox { get; set; }
-
+        
         // initialVelocityBox Validate
         [Required]
-        [Range(0, 7000, ErrorMessage = "Wrong initial velocity value")]
-        [DisallowNull]
+        [Range(0,7000,ErrorMessage = "Wrong initial velocity value")]        
         public double SelectedInitialVelocity { get; set; }
-
+        
         // shootAngelBox Validate
         [Required]
-        [Range(0, 90, ErrorMessage = "Wrong shooting angle value")]
-        [DisallowNull]
+        [Range(0,90,ErrorMessage = "Wrong shooting angle value")]        
         public int SelectedAngle { get; set; }
-
+        
         // temperatureBox Validate
         [Required]
-        [Range(-273, 5000, ErrorMessage = "Wrong temperature value")]
-        [DisallowNull]
+        [Range(-273,5000,ErrorMessage = "Wrong temperature value")]
         public int SelectedTemperature { get; set; }
-
+        
         // pressureBox Validate
         [Required]
-        [Range(0, 2500, ErrorMessage = "Wrong pressure value")]
-        [DisallowNull]
+        [Range(0,2500,ErrorMessage = "Wrong pressure value")]
         public int SelectedPressure { get; set; }
-
+        
         // humidityBox Validate
         [Required]
-        [Range(0, 100, ErrorMessage = "Wrong humidity value")]
-        [DisallowNull]
+        [Range(0,100,ErrorMessage = "Wrong humidity value")]
         public int SelectedHumidity { get; set; }
-
+        
         // airDensityBox Validate
         [Required]
-        [Range(0, 100, ErrorMessage = "Wrong air density value")]
-        [DisallowNull]
+        [Range(0,100,ErrorMessage = "Wrong air density value")]
         public double SelectedAirDensity { get; set; }
-
+        
         [Required]
-        [Range(1, 30000, ErrorMessage = "Wrong chart size value")]
-        [DisallowNull]
+        [Range(1,30000,ErrorMessage = "Wrong chart size value")]
         public int SelectedChartSize { get; set; }
-
+        
         public double BallisticCoefficient { get; set; }
 
         [Required]
-        [Range(0, 200, ErrorMessage = "Wrong wind velocity value")]
-        [DisallowNull]
+        [Range(0,200,ErrorMessage = "Wrong wind velocity value")]
         public double SelectedWindVelocity { get; set; }
     }
 }
